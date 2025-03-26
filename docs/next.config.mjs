78be19c25eecb6e3e3c4a0bb351fd5f9b443d377
tailwiinder <tailwiinder@gmail.com>
2025-03-26 06:34:52 +0530

    minor version fixes

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,9 +1,11 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-});
+})
 
-module.exports = withNextra({
+export default withNextra({
   reactStrictMode: true,
   webpack: config => {
     // Only needed if you're still using React Native Web components
